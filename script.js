bdec847427174134c1e159ff785caf37f69ab135
gguneii <guneysuleymanofa@gmail.com>
2024-09-18 22:58:21 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,398 @@
+const data = [{
+        "name": "Shirley Kuphal",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "er",
+        "price": 52,
+        "id": "1"
+    },
+    {
+        "name": "Linda Rempel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "quod",
+        "price": 95,
+        "id": "2"
+    },
+    {
+        "name": "Cary Pfeffer DVM",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "apropos",
+        "price": 25,
+        "id": "3"
+    },
+    {
+        "name": "Geoffrey O'Conner",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "phooey",
+        "price": 91,
+        "id": "4"
+    },
+    {
+        "name": "Garry Haag",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "pomelo",
+        "price": 56,
+        "id": "5"
+    },
+    {
+        "name": "Gail Fadel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Configuration",
+        "price": 20,
+        "id": "6"
+    },
+    {
+        "name": "Vivian Jacobs",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Gloves",
+        "price": 62,
+        "id": "7"
+    },
+    {
+        "name": "Derrick Herman",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "visualize",
+        "price": 11,
+        "id": "8"
+    },
+    {
+        "name": "Barbara Walker",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Loan",
+        "price": 84,
+        "id": "9"
+    },
+    {
+        "name": "Marsha Rempel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "pink",
+        "price": 93,
+        "id": "10"
+    },
+    {
+        "name": "Darryl Metz",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "wrap",
+        "price": 62,
+        "id": "11"
+    },
+    {
+        "name": "Peter Reichert",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "invoice",
+        "price": 88,
+        "id": "12"
+    },
+    {
+        "name": "Teri Gottlieb",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Bicycle",
+        "price": 64,
+        "id": "13"
+    },
+    {
+        "name": "Terry Koss",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "circuit",
+        "price": 44,
+        "id": "14"
+    },
+    {
+        "name": "Darlene Stoltenberg",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "boat",
+        "price": 90,
+        "id": "15"
+    },
+    {
+        "name": "Alfredo Kshlerin",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "payment",
+        "price": 7,
+        "id": "16"
+    },
+    {
+        "name": "Kara Kihn V",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "overriding",
+        "price": 71,
+        "id": "17"
+    },
+    {
+        "name": "Meghan Lemke DVM",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Mississippi",
+        "price": 89,
+        "id": "18"
+    },
+    {
+        "name": "Marcia Okuneva",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "siemens",
+        "price": 43,
+        "id": "19"
+    },
+    {
+        "name": "Harry Baumbach",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "SUV",
+        "price": 5,
+        "id": "20"
+    },
+    {
+        "name": "Winston Connelly III",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "transitional",
+        "price": 58,
+        "id": "21"
+    },
+    {
+        "name": "Rachael Kiehn DVM",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Lauderhill",
+        "price": 89,
+        "id": "22"
+    },
+    {
+        "name": "Olga Feil",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Music",
+        "price": 77,
+        "id": "23"
+    },
+    {
+        "name": "Matthew Mayert",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "McKenzie",
+        "price": 63,
+        "id": "24"
+    },
+    {
+        "name": "Mr. Alex Thiel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Oriental",
+        "price": 6,
+        "id": "25"
+    },
+    {
+        "name": "Heidi Kihn",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "iterate",
+        "price": 99,
+        "id": "26"
+    },
+    {
+        "name": "Ms. Andy Monahan",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "District",
+        "price": 2,
+        "id": "27"
+    },
+    {
+        "name": "Merle Abernathy Sr.",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "synthesizing",
+        "price": 59,
+        "id": "28"
+    },
+    {
+        "name": "Maxine Kuvalis",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "pro",
+        "price": 19,
+        "id": "29"
+    },
+    {
+        "name": "Lynda Zboncak",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "male",
+        "price": 43,
+        "id": "30"
+    },
+    {
+        "name": "Justin Hills II",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "budgetary",
+        "price": 99,
+        "id": "31"
+    },
+    {
+        "name": "Thelma Thiel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "foolishly",
+        "price": 37,
+        "id": "32"
+    },
+    {
+        "name": "Jeannette Hessel",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "users",
+        "price": 30,
+        "id": "33"
+    },
+    {
+        "name": "Nick Hagenes",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "BMW",
+        "price": 19,
+        "id": "34"
+    },
+    {
+        "name": "Aubrey Stracke",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "communication",
+        "price": 28,
+        "id": "35"
+    },
+    {
+        "name": "Claude Lehner",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Sedan",
+        "price": 68,
+        "id": "36"
+    },
+    {
+        "name": "Ray Pouros",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Wooden",
+        "price": 7,
+        "id": "37"
+    },
+    {
+        "name": "Mr. Angel Fritsch I",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "harum",
+        "price": 94,
+        "id": "38"
+    },
+    {
+        "name": "Joel Hagenes",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Country",
+        "price": 50,
+        "id": "39"
+    },
+    {
+        "name": "Mr. June Keebler IV",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Fresh",
+        "price": 36,
+        "id": "40"
+    },
+    {
+        "name": "Sonja Kuhlman",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Account",
+        "price": 44,
+        "id": "41"
+    },
+    {
+        "name": "Lola Connelly",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "azure",
+        "price": 20,
+        "id": "42"
+    },
+    {
+        "name": "Howard McLaughlin",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "tambour",
+        "price": 88,
+        "id": "43"
+    },
+    {
+        "name": "Spencer Toy",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "RAM",
+        "price": 47,
+        "id": "44"
+    },
+    {
+        "name": "Clifton Hagenes",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Northwest",
+        "price": 87,
+        "id": "45"
+    },
+    {
+        "name": "Hattie Satterfield",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Naira",
+        "price": 66,
+        "id": "46"
+    },
+    {
+        "name": "Loretta Hilll",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "card",
+        "price": 86,
+        "id": "47"
+    },
+    {
+        "name": "Antonio Brakus",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Bicycle",
+        "price": 13,
+        "id": "48"
+    },
+    {
+        "name": "Dr. Annette Rice",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Cyril",
+        "price": 3,
+        "id": "49"
+    },
+    {
+        "name": "Derek Kub",
+        "img": "https://loremflickr.com/640/480/animals",
+        "desc": "Computers",
+        "price": 32,
+        "id": "50"
+    }
+]
+const cards = document.querySelector('.cards')
+cards.innerHTML = ''
+data.map((item, index) => {
+    cards.innerHTML += `
+     <div class="card h-auto border overflow-hidden m-2 rounded-[10px]">
+          <div class="overflow-hidden relative">
+            <img class="w-full h-full" src="${item.img}" alt="">
+            <i onclick="addToWishlist(${index})"class="fav fa-solid color-transparent fa-heart text-2xl absolute top-3 right-3 text-white"></i>
+
+          </div>
+           <div>
+            <h3 class="font-semibold py-1 text-[20px]">${item.name}</h3>
+            <p class="font-bold py-1 text-[22px]"> $${item.price}</p>
+            <button class="text-center bg-[#212a39] w-full text-white p-3">Add to cart</button>
+           </div>
+        </div>
+    
+    `
+})
+
+const wishlist = document.getElementById('wishlist')
+
+function addToWishlist(index) {
+    const selectedItem = data[index]
+    wishlist.innerHTML += `
+    <div id="wishlist-${index}" class="card w-full h-auto flex border rounded-sm justify-between my-2">
+    <div class="flex">
+    <img class="w-20" src="${selectedItem.img}" alt="">
+    <div class="px-2"><h3 class="font-semibold py-1 text-[10px]">${selectedItem.name}</h3>
+    <p class="font-bold text-[12px]">$${selectedItem.price}</p>
+    </div>
+    </div>
+    <div><i onclick="deleteWislist(${index})" class="fa-solid cursor-pointer fa-x px-2"></i></div>
+    </div>
+   `
+   const fav = document.querySelectorAll('.fav')[index]
+   fav.style.color = 'red'; 
+}
+
+function showFavs() {
+    wishlist.classList.toggle('hidden')
+}
+
+function deleteWislist(index) {
+    const wishlistItem = document.getElementById(`wishlist-${index}`)
+    wishlistItem.remove()
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{htm,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'xsm':'400px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+}
+
